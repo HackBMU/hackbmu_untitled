760cc8e23068492a05eb8935d379d97a4c59ae5d
vinayannam <vinay1011979.7@gmail.com>
2018-04-07 13:51:24 +0530

    final project

--- main.js
+++ main.js
@@ -8,9 +8,7 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var exphbs = require('express-handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -20,6 +18,10 @@ app.set('view engine', 'handlebars');
 
 var datas = [];
 var count = 0;
+
+app.get('/', function(req, res) {
+    res.render('mainhtml');
+});
 
 io.on('connection', function(socket) {
     var datas = [];
@@ -39,32 +41,12 @@ io.on('connection', function(socket) {
 
     });
     socket.on('snap', function(data) {
-        var file = require('./test.json');
+        var file = require('./train.json');
         var fileData = JSON.parse(JSON.stringify(file));
         fileData.push(datas[datas.length - 1])
-        fs.writeFile('test.json', JSON.stringify(fileData), "utf8");
+        fs.writeFile('train.json', JSON.stringify(fileData), "utf8");
         datas = []
     })
-});
-
-var and = perceptron();
-
-var train = require('./train.json');
-var trainData = JSON.parse(JSON.stringify(train));
-
-trainData.forEach(function(da) {
-    and.train([da.a, da.b, da.c, da.d, da.e, da.f], da.label);
-});
-
-var test = require('./test.json');
-var testData = JSON.parse(JSON.stringify(test));
-
-var str = '';
-
-testData.forEach(function(da) {
-    if (and.perceive([da.a, da.b, da.c, da.d, da.e, da.f]) == '0') {
-
-    }
 });
 
 
@@ -73,46 +55,74 @@ app.get('/chatbot', function(req, res) {
     res.render('index');
 });
 
+var str = '';
+var and = perceptron();
+
+function train() {
+
+    var train = require('./train.json');
+    var trainData = JSON.parse(JSON.stringify(train));
+
+    trainData.forEach(function(da) {
+        and.train([da.a, da.b, da.c, da.d, da.e, da.f], da.label);
+    });
+}
+
+var cou = 0;
+
 app.post('/chatbot', function(req, resp) {
-    console.log(req.body.input);
-    const apiai = require('apiai');
-    const app = apiai("11c7a7fad3714ec28643becb17571427");
+    train();
+    if (cou == 4) {
+        console.log(req.body);
+        var pred = and.perceive([req.body.a, req.body.b, req.body.c, req.body.d, req.body.e, req.body.f]);
+        console.log('pred: ' + pred);
+        if (pred == 0) {
+            str = 'weather';
+            console.log('The weather outside is 67 F.');
+        } else {
+            str = 'joke';
+        }
+        const apiai = require('apiai');
+        const app = apiai("11c7a7fad3714ec28643becb17571427");
 
-    var request = app.textRequest(req.body.input, {
-        sessionId: '2'
-    });
-
-    request.on('response', function(response) {
-        var res = JSON.parse(JSON.stringify(response));
-        const Translate = require('@google-cloud/translate');
-        const projectId = 'theta-office-199904';
-        const translate = new Translate({
-            projectId: projectId,
+        var request = app.textRequest(str, {
+            sessionId: '2'
         });
-        const text = res.result.fulfillment.speech;
-        console.log(text);
-        const target = 'hi';
-        translate.translate(text, target).then(results => {
-            const translation = results[0];
-            console.log(`Text: ${text}`);
-            console.log(`Translation: ${translation}`);
-            var googleTTS = require('google-tts-api');
-            googleTTS(translation, 'hi', 1)
-                .then(function(url) {
-                    resp.send(url);
-                    console.log(url);
-                })
-                .catch(function(err) {
-                    console.error(err.stack);
-                });
-        }).catch(err => {
-            console.error('ERROR:', err);
+
+        request.on('response', function(response) {
+            var res = JSON.parse(JSON.stringify(response));
+            const Translate = require('@google-cloud/translate');
+            const projectId = 'theta-office-199904';
+            const translate = new Translate({
+                projectId: projectId,
+            });
+            const text = res.result.fulfillment.speech;
+            console.log(text);
+            const target = 'hi';
+            translate.translate(text, target).then(results => {
+                const translation = results[0];
+                console.log(`Text: ${text}`);
+                console.log(`Translation: ${translation}`);
+                var googleTTS = require('google-tts-api');
+                googleTTS(translation, 'hi', 1)
+                    .then(function(url) {
+                        resp.send(url);
+                        console.log(url);
+                    })
+                    .catch(function(err) {
+                        console.error(err.stack);
+                    });
+            }).catch(err => {
+                console.error('ERROR:', err);
+            });
         });
-    });
 
-    request.on('error', function(error) {
-        console.log(error);
-    });
+        request.on('error', function(error) {
+            console.log(error);
+        });
 
-    request.end();
+        request.end();
+        // cou = 0;
+    }
+    cou++;
 });
